cc651fbb890abbcc74ce5b9ececbe23a30be7730
André Cianfarani <andre.cianfarani@insign.fr>
2020-04-06 09:30:59 +0200

    Stash suring build build

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-PHPVERSION="7.4"
+PHPVERSION="7.3"
 
 perl -p -i -e "s|PHPVERSION=.*$|PHPVERSION=$PHPVERSION|g" Dockerfile
 docker build -t insignagency/php:php$PHPVERSION .
 
 if [[ "$(git branch |grep php$PHPVERSION)" =~ (^| )php$PHPVERSION( |$) ]];
 then
+    git stash
     git checkout php$PHPVERSION
+    git stash pop
 else
+    git stash
     git checkout -b php$PHPVERSION
+    git stash pop
 fi
 
 git diff
