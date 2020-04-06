8ab10714348edf08a85029ca4680834f35ebde90
André Cianfarani <andre.cianfarani@insign.fr>
2020-04-06 09:29:15 +0200

    New build based on branches

--- build.sh
+++ build.sh
@@ -4,15 +4,14 @@ PHPVERSION="7.4"
 perl -p -i -e "s|PHPVERSION=.*$|PHPVERSION=$PHPVERSION|g" Dockerfile
 docker build -t insignagency/php:php$PHPVERSION .
 
-if [ "$(git tag --list |grep php$PHPVERSION)" == "" ];
+if [[ "$(git branch |grep php$PHPVERSION)" =~ (^| )php$PHPVERSION( |$) ]];
 then
-    git tag php$PHPVERSION
-    echo "Pour pusher sur le remote :"
-    echo "git push origin php$PHPVERSION"
+    git checkout php$PHPVERSION
 else
-    git tag -d php$PHPVERSION
-    git tag php$PHPVERSION
-    echo "Pour pusher sur le remote :"
-    echo "git push --delete origin php$PHPVERSION"
-    echo "git push origin php$PHPVERSION"
+    git checkout -b php$PHPVERSION
 fi
+
+git diff
+
+echo "To push:"
+echo "git push -u origin php$PHPVERSION"
